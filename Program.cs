04275c5ec56b0df7335cfb5d912e6bec5389e96a
Samspudz <adammarshall11@yahoo.com>
2023-01-31 17:50:23 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * author: Adam Marshall
+ * date: 01/31/2023
+ * class: GAME 1343
+ * project: Week 2 Assignment
+ */
+
+namespace Week2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myVar = 11;
+
+            Console.WriteLine(-1 + 4 * myVar);
+            Console.WriteLine((35 + 5) % myVar);
+            Console.WriteLine(14 + -4 * 6 / 12);
+            Console.WriteLine(2 + 12 / 6 * 1 - myVar % 2);
+
+            if(myVar * myVar < 100)
+            {
+                Console.WriteLine("myVar is less than 10");
+            }
+            else 
+            {
+                Console.WriteLine("myVar is greater than 10");
+            }
+
+        }
+    }
+}
